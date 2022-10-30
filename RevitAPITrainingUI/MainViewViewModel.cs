f139e7ddf2de4702630c3d3acaf673d9bebb61ec
эмиль ахметханов <emil.ah@yandex.ru>
2022-10-30 20:17:52 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingUI/MainViewViewModel.cs
@@ -0,0 +1,96 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using RevitAPITrainingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingUI
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        //public DelegateCommand SelectCommand { get; }
+
+        public DelegateCommand SaveCommand { get; }
+
+        public List<Element> PickedObjects { get; } = new List<Element>();
+
+        public List<WallFunction> WallSystems { get; } = new List<WallFunction>();
+
+        public WallType SelectedWallSystem { get; set; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+            PickedObjects = SelectionUtils.PickObjects(commandData);
+            WallSystems = WallsUtils.GetWallSystems(commandData); 
+            //SelectCommand = new DelegateCommand(OnSelectCommand);
+        }
+
+        private void OnSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            if (PickedObjects.Count == 0 || SelectedWallSystem == null)
+                return;
+
+            using (var ts = new Transaction(doc, "Set system type"))
+            {
+                ts.Start();
+
+                foreach (var pickedObject in PickedObjects)
+                {
+                    if (pickedObject is Wall)
+                    {
+                        var oWall = pickedObject as Wall;
+                        oWall.SetSystemType(SelectedWallSystem.Id);              
+                    }
+                }
+
+                ts.Commit();
+            }
+            RaiseCloseRequest();
+        }
+
+        public event EventHandler CloseRequest;
+        private void RaiseCloseRequest() 
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+        //public event EventHandler HideRequest;
+
+        //private void RaiserHideRequest() 
+        //{
+        //    HideRequest?.Invoke(this, EventArgs.Empty);
+        //}
+
+        //public event EventHandler ShowRequest;
+
+        //private void RaiseShowRequest()
+        //{
+        //    ShowRequest?.Invoke(this, EventArgs.Empty);
+        //}
+
+        //private void OnSelectCommand()
+        //{
+        //    RaiserHideRequest();
+        //    Element oElement = SelectionUtils.PickObject(_commandData);
+
+        //    TaskDialog.Show("Сообщение", $"ID: {oElement.Id}");
+
+        //    RaiseShowRequest();
+        //}
+
+    }
+}
---
+++ RevitAPITrainingUI/UserControl1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RevitAPITrainingUI
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : Window
+    {
+        public UserControl1(ExternalCommandData commandData)
+        {
+            InitializeComponent();
+            MainViewViewModel vm = new MainViewViewModel(commandData);
+            vm.CloseRequest += (s, e) => this.Close();
+            //vm.ShowRequest += (s, e) => this.Show();
+            DataContext = vm;
+        }
+    }
+}
